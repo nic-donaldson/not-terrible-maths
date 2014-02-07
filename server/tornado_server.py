6c1f6282b47d4558200cf0a8471b0a772abdb6a8
Nicholas Donaldson <nic.donaldson.0@gmail.com>
2014-02-07 20:34:01 +1100

    removed global variables and replaced with database dictionary

--- server/tornado_server.py
+++ server/tornado_server.py
@@ -3,72 +3,66 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
-prior = None
-pairs = {}
-
 class EchoWebSocket(tornado.websocket.WebSocketHandler):
+	def initialize(self, database):
+		self.database = database
 
 	def open(self):
-		global prior
-		global pairs
 
-		if prior == None:
+		if database['prior'] == None:
 			print("New user connected")
-			prior = self
+			database['prior'] = self
 		else:
 			print("New user connected, connecting with previous user")
-			pairs[prior] = self
-			pairs[self] = prior
-			prior.write_message("1:Connected to another!")
+			database['pairs'][database['prior']] = self
+			database['pairs'][self] = database['prior']
+			database['prior'].write_message("1:Connected to another!")
 			self.write_message("2:Connected to another!")
-			prior = None
+			database['prior'] = None
 
 	def on_message(self, message):
-		global prior
-		global pairs
-
-		if self in pairs:
-			pairs[self].write_message(message)
+		if self in database['pairs']:
+			database['pairs'][self].write_message(message)
 		else:
 			self.write_message("No friend yet, :(")
 
 
 	def on_close(self):
-		global prior
-		global pairs
 
-		if self in pairs:
-			pairs[self].write_message("Your friend has disconnected")
+		if self in database['pairs']:
+			database['pairs'][self].write_message("Your friend has disconnected")
 			# if there is no-one else to talk to
-			if prior == None:
-				prior = pairs[self]
-				del pairs[self]
-				del pairs[prior]
+			if database['prior'] == None:
+				database['prior'] = database['pairs'][self]
+				del database['pairs'][self]
+				del database['pairs'][database['prior']]
 			# if there is someone else to talk to!
 			else:
-				old_friend = pairs[self]
-				del pairs[self]
-				pairs[old_friend] = prior
-				pairs[prior] = old_friend
-				prior = None
+				old_friend = database['pairs'][self]
+				del database['pairs'][self]
+				database['pairs'][old_friend] = database['prior']
+				database['pairs'][database['prior']] = old_friend
+				database['prior'] = None
 
 				old_friend.write_message("New friend connected")
-				pairs[old_friend].write_message("Friend connected!")
+				database['pairs'][old_friend].write_message("Friend connected!")
 		else:
-			if self == prior:
-				prior = None
+			if self == database['prior']:
+				database['prior'] = None
 
 class RootHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.redirect("/static/websockets.html")
 
 
+database = {'pairs' : {}, 'prior' : None}
+
 settings = {
 	"static_path": os.path.join(os.path.dirname(__file__), "../client")
 }
 
 application = tornado.web.Application([
-	(r"/connect", EchoWebSocket),
+	(r"/connect", EchoWebSocket, dict(database=database)),
 	(r"/", RootHandler),
 ], **settings)
 
